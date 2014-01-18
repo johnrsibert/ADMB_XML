8066eb5e959e4226718e3af7ef5fac09fd4d89a8
johnrsibert <sibert@hawaii.edu>
2014-01-17 14:00:45 -1000

    initial commit of source code

---
+++ ADMB_XMLDoc.d
@@ -0,0 +1,488 @@
+ADMB_XMLDoc.o: ADMB_XMLDoc.cpp ADMB_XMLDoc.h \
+ /home/jsibert/admb/trunk/build/dist/include/fvar.hpp /usr/include/math.h \
+ /usr/include/features.h /usr/include/x86_64-linux-gnu/bits/predefs.h \
+ /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/bits/huge_val.h \
+ /usr/include/x86_64-linux-gnu/bits/huge_valf.h \
+ /usr/include/x86_64-linux-gnu/bits/huge_vall.h \
+ /usr/include/x86_64-linux-gnu/bits/inf.h \
+ /usr/include/x86_64-linux-gnu/bits/nan.h \
+ /usr/include/x86_64-linux-gnu/bits/mathdef.h \
+ /usr/include/x86_64-linux-gnu/bits/mathcalls.h \
+ /usr/include/x86_64-linux-gnu/bits/mathinline.h /usr/include/unistd.h \
+ /usr/include/x86_64-linux-gnu/bits/posix_opt.h \
+ /usr/include/x86_64-linux-gnu/bits/environments.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/local/gcc-4.8.0/lib/gcc/x86_64-unknown-linux-gnu/4.8.0/include/stddef.h \
+ /usr/include/x86_64-linux-gnu/bits/confname.h /usr/include/getopt.h \
+ /usr/include/fcntl.h /usr/include/x86_64-linux-gnu/bits/fcntl.h \
+ /usr/include/x86_64-linux-gnu/sys/types.h /usr/include/time.h \
+ /usr/include/endian.h /usr/include/x86_64-linux-gnu/bits/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/byteswap.h \
+ /usr/include/x86_64-linux-gnu/sys/select.h \
+ /usr/include/x86_64-linux-gnu/bits/select.h \
+ /usr/include/x86_64-linux-gnu/bits/sigset.h \
+ /usr/include/x86_64-linux-gnu/bits/time.h \
+ /usr/include/x86_64-linux-gnu/sys/sysmacros.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes.h \
+ /usr/include/x86_64-linux-gnu/bits/uio.h \
+ /usr/include/x86_64-linux-gnu/bits/stat.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/fstream \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/istream \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/ios \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/iosfwd \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/x86_64-unknown-linux-gnu/bits/c++config.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/x86_64-unknown-linux-gnu/bits/os_defines.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/x86_64-unknown-linux-gnu/bits/cpu_defines.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/bits/stringfwd.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/bits/memoryfwd.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/bits/postypes.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/cwchar /usr/include/wchar.h \
+ /usr/include/stdio.h \
+ /usr/local/gcc-4.8.0/lib/gcc/x86_64-unknown-linux-gnu/4.8.0/include/stdarg.h \
+ /usr/include/x86_64-linux-gnu/bits/wchar.h /usr/include/xlocale.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/exception \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/bits/atomic_lockfree_defines.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/bits/char_traits.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/bits/stl_algobase.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/bits/functexcept.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/bits/exception_defines.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/bits/cpp_type_traits.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/ext/type_traits.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/ext/numeric_traits.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/bits/stl_pair.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/bits/move.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/bits/concept_check.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/bits/stl_iterator_base_types.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/bits/stl_iterator_base_funcs.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/debug/debug.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/bits/stl_iterator.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/bits/localefwd.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/x86_64-unknown-linux-gnu/bits/c++locale.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/clocale /usr/include/locale.h \
+ /usr/include/x86_64-linux-gnu/bits/locale.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/cctype /usr/include/ctype.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/bits/ios_base.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/ext/atomicity.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/x86_64-unknown-linux-gnu/bits/gthr.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/x86_64-unknown-linux-gnu/bits/gthr-default.h \
+ /usr/include/pthread.h /usr/include/sched.h \
+ /usr/include/x86_64-linux-gnu/bits/sched.h \
+ /usr/include/x86_64-linux-gnu/bits/timex.h \
+ /usr/include/x86_64-linux-gnu/bits/setjmp.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/x86_64-unknown-linux-gnu/bits/atomic_word.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/bits/locale_classes.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/string \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/bits/allocator.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/x86_64-unknown-linux-gnu/bits/c++allocator.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/ext/new_allocator.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/new \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/bits/ostream_insert.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/bits/cxxabi_forced.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/bits/stl_function.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/backward/binders.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/bits/range_access.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/bits/basic_string.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/bits/basic_string.tcc \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/bits/locale_classes.tcc \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/streambuf \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/bits/streambuf.tcc \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/bits/basic_ios.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/bits/locale_facets.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/cwctype /usr/include/wctype.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/x86_64-unknown-linux-gnu/bits/ctype_base.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/bits/streambuf_iterator.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/x86_64-unknown-linux-gnu/bits/ctype_inline.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/bits/locale_facets.tcc \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/bits/basic_ios.tcc \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/ostream \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/bits/ostream.tcc \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/bits/istream.tcc \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/bits/codecvt.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/cstdio /usr/include/libio.h \
+ /usr/include/_G_config.h /usr/include/x86_64-linux-gnu/bits/stdio_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/sys_errlist.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/x86_64-unknown-linux-gnu/bits/basic_file.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/x86_64-unknown-linux-gnu/bits/c++io.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/bits/fstream.tcc \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/iostream \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/iomanip \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/sstream \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/bits/sstream.tcc \
+ /home/jsibert/admb/trunk/build/dist/include/dfpool.h \
+ /usr/include/stdlib.h /usr/include/x86_64-linux-gnu/bits/waitflags.h \
+ /usr/include/x86_64-linux-gnu/bits/waitstatus.h /usr/include/alloca.h \
+ /home/jsibert/admb/trunk/build/dist/include/factors.h \
+ /home/jsibert/admb/trunk/build/dist/include/adstring.hpp \
+ /home/jsibert/admb/trunk/build/dist/include/clist.h \
+ /usr/include/string.h /usr/include/x86_64-linux-gnu/sys/time.h \
+ /home/jsibert/admb/trunk/build/dist/include/admodel.h \
+ /home/jsibert/admb/trunk/build/dist/include/cifstrem.h \
+ /home/jsibert/admb/trunk/build/dist/include/param_init_bounded_number_matrix.h \
+ /home/jsibert/admb/trunk/build/dist/include/admodel.h \
+ /usr/include/libxml2/libxml/tree.h \
+ /usr/include/libxml2/libxml/xmlversion.h \
+ /usr/include/libxml2/libxml/xmlexports.h \
+ /usr/include/libxml2/libxml/xmlstring.h \
+ /usr/include/libxml2/libxml/xmlregexp.h \
+ /usr/include/libxml2/libxml/dict.h \
+ /usr/include/libxml2/libxml/xmlmemory.h \
+ /usr/include/libxml2/libxml/threads.h \
+ /usr/include/libxml2/libxml/globals.h \
+ /usr/include/libxml2/libxml/parser.h /usr/include/libxml2/libxml/hash.h \
+ /usr/include/libxml2/libxml/valid.h \
+ /usr/include/libxml2/libxml/xmlerror.h \
+ /usr/include/libxml2/libxml/list.h \
+ /usr/include/libxml2/libxml/xmlautomata.h \
+ /usr/include/libxml2/libxml/entities.h \
+ /usr/include/libxml2/libxml/encoding.h /usr/include/iconv.h \
+ /usr/include/libxml2/libxml/xmlIO.h /usr/include/libxml2/libxml/SAX.h \
+ /usr/include/libxml2/libxml/xlink.h /usr/include/libxml2/libxml/SAX2.h \
+ /usr/include/libxml2/libxml/xpath.h \
+ /usr/include/libxml2/libxml/xpathInternals.h \
+ /usr/local/gcc-4.8.0/include/c++/4.8.0/stdexcept
+
+ADMB_XMLDoc.h:
+
+/home/jsibert/admb/trunk/build/dist/include/fvar.hpp:
+
+/usr/include/math.h:
+
+/usr/include/features.h:
+
+/usr/include/x86_64-linux-gnu/bits/predefs.h:
+
+/usr/include/x86_64-linux-gnu/sys/cdefs.h:
+
+/usr/include/x86_64-linux-gnu/bits/wordsize.h:
+
+/usr/include/x86_64-linux-gnu/gnu/stubs.h:
+
+/usr/include/x86_64-linux-gnu/gnu/stubs-64.h:
+
+/usr/include/x86_64-linux-gnu/bits/huge_val.h:
+
+/usr/include/x86_64-linux-gnu/bits/huge_valf.h:
+
+/usr/include/x86_64-linux-gnu/bits/huge_vall.h:
+
+/usr/include/x86_64-linux-gnu/bits/inf.h:
+
+/usr/include/x86_64-linux-gnu/bits/nan.h:
+
+/usr/include/x86_64-linux-gnu/bits/mathdef.h:
+
+/usr/include/x86_64-linux-gnu/bits/mathcalls.h:
+
+/usr/include/x86_64-linux-gnu/bits/mathinline.h:
+
+/usr/include/unistd.h:
+
+/usr/include/x86_64-linux-gnu/bits/posix_opt.h:
+
+/usr/include/x86_64-linux-gnu/bits/environments.h:
+
+/usr/include/x86_64-linux-gnu/bits/types.h:
+
+/usr/include/x86_64-linux-gnu/bits/typesizes.h:
+
+/usr/local/gcc-4.8.0/lib/gcc/x86_64-unknown-linux-gnu/4.8.0/include/stddef.h:
+
+/usr/include/x86_64-linux-gnu/bits/confname.h:
+
+/usr/include/getopt.h:
+
+/usr/include/fcntl.h:
+
+/usr/include/x86_64-linux-gnu/bits/fcntl.h:
+
+/usr/include/x86_64-linux-gnu/sys/types.h:
+
+/usr/include/time.h:
+
+/usr/include/endian.h:
+
+/usr/include/x86_64-linux-gnu/bits/endian.h:
+
+/usr/include/x86_64-linux-gnu/bits/byteswap.h:
+
+/usr/include/x86_64-linux-gnu/sys/select.h:
+
+/usr/include/x86_64-linux-gnu/bits/select.h:
+
+/usr/include/x86_64-linux-gnu/bits/sigset.h:
+
+/usr/include/x86_64-linux-gnu/bits/time.h:
+
+/usr/include/x86_64-linux-gnu/sys/sysmacros.h:
+
+/usr/include/x86_64-linux-gnu/bits/pthreadtypes.h:
+
+/usr/include/x86_64-linux-gnu/bits/uio.h:
+
+/usr/include/x86_64-linux-gnu/bits/stat.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/fstream:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/istream:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/ios:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/iosfwd:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/x86_64-unknown-linux-gnu/bits/c++config.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/x86_64-unknown-linux-gnu/bits/os_defines.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/x86_64-unknown-linux-gnu/bits/cpu_defines.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/bits/stringfwd.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/bits/memoryfwd.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/bits/postypes.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/cwchar:
+
+/usr/include/wchar.h:
+
+/usr/include/stdio.h:
+
+/usr/local/gcc-4.8.0/lib/gcc/x86_64-unknown-linux-gnu/4.8.0/include/stdarg.h:
+
+/usr/include/x86_64-linux-gnu/bits/wchar.h:
+
+/usr/include/xlocale.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/exception:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/bits/atomic_lockfree_defines.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/bits/char_traits.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/bits/stl_algobase.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/bits/functexcept.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/bits/exception_defines.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/bits/cpp_type_traits.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/ext/type_traits.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/ext/numeric_traits.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/bits/stl_pair.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/bits/move.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/bits/concept_check.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/bits/stl_iterator_base_types.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/bits/stl_iterator_base_funcs.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/debug/debug.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/bits/stl_iterator.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/bits/localefwd.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/x86_64-unknown-linux-gnu/bits/c++locale.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/clocale:
+
+/usr/include/locale.h:
+
+/usr/include/x86_64-linux-gnu/bits/locale.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/cctype:
+
+/usr/include/ctype.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/bits/ios_base.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/ext/atomicity.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/x86_64-unknown-linux-gnu/bits/gthr.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/x86_64-unknown-linux-gnu/bits/gthr-default.h:
+
+/usr/include/pthread.h:
+
+/usr/include/sched.h:
+
+/usr/include/x86_64-linux-gnu/bits/sched.h:
+
+/usr/include/x86_64-linux-gnu/bits/timex.h:
+
+/usr/include/x86_64-linux-gnu/bits/setjmp.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/x86_64-unknown-linux-gnu/bits/atomic_word.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/bits/locale_classes.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/string:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/bits/allocator.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/x86_64-unknown-linux-gnu/bits/c++allocator.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/ext/new_allocator.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/new:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/bits/ostream_insert.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/bits/cxxabi_forced.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/bits/stl_function.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/backward/binders.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/bits/range_access.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/bits/basic_string.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/bits/basic_string.tcc:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/bits/locale_classes.tcc:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/streambuf:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/bits/streambuf.tcc:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/bits/basic_ios.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/bits/locale_facets.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/cwctype:
+
+/usr/include/wctype.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/x86_64-unknown-linux-gnu/bits/ctype_base.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/bits/streambuf_iterator.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/x86_64-unknown-linux-gnu/bits/ctype_inline.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/bits/locale_facets.tcc:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/bits/basic_ios.tcc:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/ostream:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/bits/ostream.tcc:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/bits/istream.tcc:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/bits/codecvt.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/cstdio:
+
+/usr/include/libio.h:
+
+/usr/include/_G_config.h:
+
+/usr/include/x86_64-linux-gnu/bits/stdio_lim.h:
+
+/usr/include/x86_64-linux-gnu/bits/sys_errlist.h:
+
+/usr/include/x86_64-linux-gnu/bits/stdio.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/x86_64-unknown-linux-gnu/bits/basic_file.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/x86_64-unknown-linux-gnu/bits/c++io.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/bits/fstream.tcc:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/iostream:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/iomanip:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/sstream:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/bits/sstream.tcc:
+
+/home/jsibert/admb/trunk/build/dist/include/dfpool.h:
+
+/usr/include/stdlib.h:
+
+/usr/include/x86_64-linux-gnu/bits/waitflags.h:
+
+/usr/include/x86_64-linux-gnu/bits/waitstatus.h:
+
+/usr/include/alloca.h:
+
+/home/jsibert/admb/trunk/build/dist/include/factors.h:
+
+/home/jsibert/admb/trunk/build/dist/include/adstring.hpp:
+
+/home/jsibert/admb/trunk/build/dist/include/clist.h:
+
+/usr/include/string.h:
+
+/usr/include/x86_64-linux-gnu/sys/time.h:
+
+/home/jsibert/admb/trunk/build/dist/include/admodel.h:
+
+/home/jsibert/admb/trunk/build/dist/include/cifstrem.h:
+
+/home/jsibert/admb/trunk/build/dist/include/param_init_bounded_number_matrix.h:
+
+/home/jsibert/admb/trunk/build/dist/include/admodel.h:
+
+/usr/include/libxml2/libxml/tree.h:
+
+/usr/include/libxml2/libxml/xmlversion.h:
+
+/usr/include/libxml2/libxml/xmlexports.h:
+
+/usr/include/libxml2/libxml/xmlstring.h:
+
+/usr/include/libxml2/libxml/xmlregexp.h:
+
+/usr/include/libxml2/libxml/dict.h:
+
+/usr/include/libxml2/libxml/xmlmemory.h:
+
+/usr/include/libxml2/libxml/threads.h:
+
+/usr/include/libxml2/libxml/globals.h:
+
+/usr/include/libxml2/libxml/parser.h:
+
+/usr/include/libxml2/libxml/hash.h:
+
+/usr/include/libxml2/libxml/valid.h:
+
+/usr/include/libxml2/libxml/xmlerror.h:
+
+/usr/include/libxml2/libxml/list.h:
+
+/usr/include/libxml2/libxml/xmlautomata.h:
+
+/usr/include/libxml2/libxml/entities.h:
+
+/usr/include/libxml2/libxml/encoding.h:
+
+/usr/include/iconv.h:
+
+/usr/include/libxml2/libxml/xmlIO.h:
+
+/usr/include/libxml2/libxml/SAX.h:
+
+/usr/include/libxml2/libxml/xlink.h:
+
+/usr/include/libxml2/libxml/SAX2.h:
+
+/usr/include/libxml2/libxml/xpath.h:
+
+/usr/include/libxml2/libxml/xpathInternals.h:
+
+/usr/local/gcc-4.8.0/include/c++/4.8.0/stdexcept:
